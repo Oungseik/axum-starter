0751535a25bc7072b4dcd28e68d40f732d7b8b9d
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-07-11 07:51:54 +0630

    feat: configure opentelemetry manually

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code)]
+use std::error::Error;
 
 mod app;
 mod config;
@@ -6,13 +6,54 @@ mod config;
 use app::create_app;
 use config::get_config;
 use dotenv::dotenv;
-use std::error::Error;
+use opentelemetry::trace::TracerProvider;
+use opentelemetry_otlp::SpanExporter;
+use opentelemetry_sdk::Resource;
+use opentelemetry_sdk::trace::SdkTracerProvider;
 use tokio::net::TcpListener;
+use tracing::{info, span};
+use tracing_subscriber::Registry;
+use tracing_subscriber::prelude::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
-    let _guard = init_tracing_opentelemetry::tracing_subscriber_ext::init_subscribers()?;
+
+    let resource = Resource::builder()
+        .with_service_name("expenses_tracker")
+        .build();
+    let exporter = SpanExporter::builder().with_http().build()?;
+
+    let provider = SdkTracerProvider::builder()
+        .with_batch_exporter(exporter)
+        .with_resource(resource)
+        .build();
+
+    let tracer = provider.tracer("expenses_tracker_tracer");
+
+    #[cfg(debug_assertions)]
+    {
+        let telemetry = tracing_opentelemetry::layer().with_tracer(tracer.clone());
+        let fmt_layer = tracing_subscriber::fmt::layer()
+            .with_target(false)
+            .with_thread_ids(true)
+            .with_line_number(true)
+            .with_level(true);
+
+        let subscriber = Registry::default().with(telemetry).with(fmt_layer);
+        tracing::subscriber::set_global_default(subscriber)?;
+    }
+
+    #[cfg(not(debug_assertions))]
+    {
+        let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+        let subscriber = Registry::default().with(telemetry);
+        tracing::subscriber::set_global_default(subscriber)?;
+    }
+
+    let root = span!(tracing::Level::TRACE, "app_start", work_units = 2);
+    let _enter = root.enter();
+    info!("Start the server");
 
     let app = create_app().await?;
     let listener = TcpListener::bind(get_config().address.as_str()).await?;
